clear gradients per batch, should fix exploding gradients issue

chessgpt/main.py
@@ -1,15 +1,14 @@
-from chess import pgn
 import torch
 import chessgpt.model.config as config
 from torch.utils.data import DataLoader
-from chessgpt.model import tokenizer, transformer as t
+from chessgpt.model import transformer as t
 from chessgpt.model.data import PGNDataset
 from datetime import datetime
 
 path = "data/Carlsen.pgn"
 
 device = torch.device("mps")
-dataset = PGNDataset(path, device, max_games=100)
+dataset = PGNDataset(path, device, max_games=200)
 dataloader = DataLoader(
     dataset=dataset,
     batch_size=config.batch_size,
@@ -20,15 +19,17 @@ model.to(device)
 model.train()
 
 loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-6)
+# loss_fn = torch.nn.Loss
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
 for epoch in range(config.num_epochs):
     print(f"--- Epoch {epoch} ---")
     model.train()
-    optimizer.zero_grad()
     for i, (input, target) in enumerate(dataloader):
-        input.to(device)
-        target.to(device)
+        optimizer.zero_grad()
+        # input.to(device)
+        # target.to(device)
         output = model(input)
         # Reshape output and target for CrossEntropyLoss
         output = output.view(-1, output.size(-1))  # (batch*seq_len, vocab_size)
@@ -36,7 +37,7 @@ for epoch in range(config.num_epochs):
         loss = loss_fn(output, target)
         loss.backward()
         optimizer.step()
-        # print(f"Epoch {epoch}: Loss = {loss.item()}")
+        # scheduler.step()
 
         if i % 100 == 0:
             model.eval()
@@ -45,7 +46,7 @@ for epoch in range(config.num_epochs):
                 val_output = val_output.view(-1, val_output.size(-1))
                 val_target = target.view(-1)
                 val_loss = loss_fn(val_output, val_target)
-                print(f"Epoch {epoch} | Round {i}: Loss = {loss.item()} Validation Loss = {val_loss.item()}")
+                print(f"Epoch {epoch} | Round {i}: Loss = {loss.item():.5f} Validation Loss = {val_loss.item():.5f}")
             model.train()
-
-torch.save(model.state_dict(), f"model-{datetime.now(): %Y-%m-%d-%H-%M-%S}.pth")
+    if epoch % 2 == 0 and config.max_games > 500:
+        torch.save(model.state_dict(), f"model-{datetime.now(): %Y-%m-%d-%H-%M-%S}-epoch-{epoch}.pth")