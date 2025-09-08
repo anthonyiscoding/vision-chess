2e128d0ef64643ba40c14214335a92a554ce3655
anthonyiscoding <142696453+anthonyiscoding@users.noreply.github.com>
2025-09-08 17:18:37 -0500

    switch to torch's built in RMSNorm

--- vision/model/transformer.py
+++ vision/model/transformer.py
@@ -5,23 +5,12 @@ from torchtune.modules import FeedForward
 from vision.model.tokenizer import special_tokens_to_embeddings
 
 
-class RMSNorm(nn.Module):
-    def __init__(self, dim, eps=1e-6):
-        super().__init__()
-        self.eps = eps
-        self.scale = nn.Parameter(torch.ones(dim))
-
-    def forward(self, x):
-        normed = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
-        return self.scale * normed
-
-
 class PreNormTransformerLayer(nn.Module):
     def __init__(self, attn, mlp, dim):
         super().__init__()
-        self.norm1 = RMSNorm(dim)
+        self.norm1 = nn.RMSNorm(dim)
         self.attn = attn
-        self.norm2 = RMSNorm(dim)
+        self.norm2 = nn.RMSNorm(dim)
         self.mlp = mlp
 
     def forward(self, x):
@@ -74,7 +63,7 @@ class ChessModel(L.LightningModule):
             ]
         )
 
-        self.final_norm = RMSNorm(self.hparams["emb_dim"])
+        self.final_norm = nn.RMSNorm(self.hparams["emb_dim"])
         self.out_head = nn.Linear(
             self.hparams["emb_dim"], self.hparams["vocabulary_size"], bias=False
         )
